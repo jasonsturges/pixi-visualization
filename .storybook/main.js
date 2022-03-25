54605ba7763f9ff420b4e7df0c7c303a49a2a582
Jason Sturges <jason@jasonsturges.com>
2022-03-25 00:04:53 -0500

    Storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 module.exports = {
   "stories": [
     "../packages/**/*.stories.mdx",
@@ -7,5 +9,12 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/html"
+  "framework": "@storybook/html",
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/pixi-visualization-core": path.resolve(__dirname, "../packages/pixi-visualization-core/src")
+    }
+    return config;
+  }
 }
